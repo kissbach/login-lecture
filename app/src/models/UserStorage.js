9dc67349bad6cd1b0198ed8978eb9b2a99b5f14c
kalpa7979 <kissbach79@naver.com>
2022-05-17 21:21:14 +0900

    UserStorage 모델 구현

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,23 @@
+"user strict";
+
+
+class UserStorage {
+    static #users = {
+        id : ['kissbach', 'lkh0918', 'ldhfly'],
+        psword : ['1234', '1111', '2222'],
+        name : ['우리밋', '나개발', '김팀장'],
+    };
+    
+    static getUsers(...fields) {
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field) => {
+            if(users.hasOwnProperty(field)) {
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        return newUsers;
+    }
+}
+
+module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,9 +1,7 @@
 'use strict';
 
-const users = {
-    id : ['kissbach', 'lkh0918', 'ldhfly'],
-    psword : ['1234', '1111', '2222'],
-};
+const UserStorage = require('../../models/UserStorage');
+
 
 
 
@@ -21,20 +19,20 @@ const process = {
     login : (req, res) => {
         const id = req.body.id,
             psword = req.body.psword;
-        
+       
+      const users = UserStorage.getUsers('id', 'psword');
+
+     const response = {};       
      if (users.id.includes(id)) {
          const idx = users.id.indexOf(id);
          if(users.psword[idx] === psword) {
-             return res.json ({
-                 success:true,
-             });
+             response.success = true;
+             return res.json(response);
          }
      }
-
-     return res.json ({
-         success:false,
-         msg:"로그인에 실패하셨습니다.",
-      });
+     response.success = false;
+     response.msg = "로그인에 실패하셨습니다.";
+     return res.json(response);
     },
 };
 
