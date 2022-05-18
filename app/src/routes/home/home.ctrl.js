bec52f8e9e03e3bc4fbb547d279115542e3f0079
kalpa7979 <kissbach79@naver.com>
2022-05-18 20:56:47 +0900

    회원가입 페이지 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -12,6 +12,10 @@ const output = {
     login : (req, res) => {
         res.render('home/login');
     },
+
+    register : (req, res) => {
+        res.render('home/register');
+    },
 };
 
 const process = {
