9f36afa83a477694c5124ad2fb1609e70e6399a2
kalpa7979 <kissbach79@naver.com>
2022-10-22 09:49:53 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -13,6 +13,7 @@ const home = require("./src/routes/home")
 //앱세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 
 app.use("/", home); // 미들웨어 등록 
